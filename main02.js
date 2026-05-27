180459be8570463c95b98afbb01163720e399176
tunyarzartoe-coding <htunyarzar2001@gmail.com>
2026-05-27 11:45:08 +0900

    [done] js-ex1

--- main02.js
+++ main02.js
@@ -1,1 +1,19 @@
 ﻿'use strict';
+// [課題02] 
+// 下図のようなPrompt ダイアログを表示し，「yes」と入力して[OK]ボタンが押された場合は「yes が入力されま
+// した」，「no」と入力して[OK]ボタンが押された場合は「no が入力されました」，それ以外の場合は「yes かno を
+// 入力して[OK]ボタンを押してください」と記載された Alert ダイアログを表示するよう，main02.js のソース
+// コードを完成させなさい。 
+// 【キーワード】prompt 
+ 
+// [Prompt ダイアログ] 
+
+let input = prompt("yes か no を入力してください");
+
+if (input === "yes") {
+    alert("yes が入力されました");  
+} else if (input === "no") {
+    alert("no が入力されました");
+} else {
+    alert("yes か no を入力して[OK]ボタンを押してください");
+}   
