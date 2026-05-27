8c98c5de18c92c4d623956ce3561c98c5b7cde6d
tunyarzartoe-coding <htunyarzar2001@gmail.com>
2026-05-27 12:25:19 +0900

    [done] js-ex04

---
+++ js-ex04/main04.js
@@ -0,0 +1,13 @@
+﻿'use strict';
+// 以下のような Confirm ダイアログ表示し，[OK]ボタンが押される度に次の Confirm ダイアログを表示し続け，
+// [キャンセル]ボタンが押されると Confirm ダイアログの表示を終了するよう，main04.js のソースコードを完
+// 成させなさい。 
+// 【キーワード】break 
+let count = 0;
+while (true) {
+  const result = confirm(`${count + 1} 回目のダイアログ表示。次のダイアログを表示しますか？（[キャンセル]で終了）`);
+    if (!result) {  
+        break;  
+    }
+    count++;
+}
