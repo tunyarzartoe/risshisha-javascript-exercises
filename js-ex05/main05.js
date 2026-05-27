d8fd06ace1580463da3831ea54c253173db18707
tunyarzartoe-coding <htunyarzar2001@gmail.com>
2026-05-27 12:36:16 +0900

    [done] js-ex05

---
+++ js-ex05/main05.js
@@ -0,0 +1,14 @@
+﻿'use strict';
+
+// while文を使用すること
+// コンソールに下図のような出力をするよう，main05.js のソースコードを完成させなさい。 
+// 【キーワード】continue 
+// 5 回目」が表示されていない
+let count = 0;
+while (count < 10) {
+    count++;
+    if (count === 5) {
+        continue;
+    }
+    console.log(`${count} 回目`);
+}
--- main05.js
+++
@@ -1,3 +0,0 @@
-﻿'use strict';
-
-// while文を使用すること
