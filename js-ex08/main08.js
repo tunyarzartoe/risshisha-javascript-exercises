41b45684ba7d7ef0a2d347e7eaa159f69ae52daf
tunyarzartoe-coding <htunyarzar2001@gmail.com>
2026-05-29 13:38:47 +0900

    [done] js-ex08"

--- js-ex08/main08.js
+++ js-ex08/main08.js
@@ -4,8 +4,13 @@
 // 引数year：西暦を表す整数
 // 返却値：引数yearが，うるう年なら'うるう年'，うるう年でなければ'平年'
 
-
-
+function leapYear(leapYear) {
+  if (leapYear % 4 === 0 && leapYear % 100 !== 0 || leapYear % 400 === 0) {
+    return 'うるう年';    
+  } else {
+    return '平年';
+  }
+}  
 
 let i = 1900;
 console.log(`${i}年は${leapYear(i)}です`);
