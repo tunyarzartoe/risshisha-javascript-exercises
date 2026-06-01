5f309da8a46379ad5e9ede5f6d5d6022b716c2a7
tunyarzartoe-coding <htunyarzar2001@gmail.com>
2026-06-01 13:46:00 +0900

    [done] js-ex10-02

--- js-ex10/main10-2.js
+++ js-ex10/main10-2.js
@@ -4,5 +4,8 @@ const numbers = [10, 20, 30, 40, 50];
 
 // 配列numbersの先頭の要素を取り除く
 
-
+numbers.shift();
 // ここに，for文を使用して配列numbersの要素を順にコンソールに表示するコードを追加
+numbers.forEach((number) => {
+  console.log(number);
+});
