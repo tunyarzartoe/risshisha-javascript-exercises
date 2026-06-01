bb65517695f7dc9b16248f36ac35ae193eee3c03
tunyarzartoe-coding <htunyarzar2001@gmail.com>
2026-06-01 13:42:29 +0900

    [done] js-ex10-01

--- js-ex10/main10-1.js
+++ js-ex10/main10-1.js
@@ -4,3 +4,8 @@ const numbers = [10, 20, 30, 40, 50];
 
 // ここに，for文を使用して配列numbersの要素を順にコンソールに表示するコードを追加
 // 但し，配列numbersの先頭の要素はコンソールに表示しないこと
+numbers.shift();
+
+for (let i = 0; i < numbers.length; i++) {
+  console.log(numbers[i]);
+}
