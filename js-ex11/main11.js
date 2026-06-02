1f7b870e3a601803354534146a6d3e8ffc37bbbc
tunyarzartoe-coding <htunyarzar2001@gmail.com>
2026-06-02 10:41:55 +0900

    [done] js-ex11

---
+++ js-ex11/main11.js
@@ -0,0 +1,11 @@
+﻿'use strict';
+
+const processes = ['要件定義', '外部設計', '内部設計', 'プログラミング', 'テスト', '運用・保守'];
+const title = document.getElementById('title');
+title.textContent = 'システム開発の流れ';
+const list = document.getElementById('process');
+for (const process of processes) {
+  const item = document.createElement('li');
+  item.textContent = process;
+  list.appendChild(item);
+}
