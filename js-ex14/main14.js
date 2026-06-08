71ec4a02edfd79dc1c8a8bc2cfb0733520ec7eeb
tunyarzartoe-coding <htunyarzar2001@gmail.com>
2026-06-08 09:53:40 +0900

    [done] js-ex14

---
+++ js-ex14/main14.js
@@ -0,0 +1,8 @@
+﻿'use strict';
+// ブラウザの[検索]ボタンをクリックすると，下図のような出力をするよう，main14.js のソースコードを完成さ
+// せなさい。 
+// 【キーワード】addEventListener，click イベント 
+
+document.getElementById('search').addEventListener('click', () => {
+    document.getElementById('msg').textContent = '検索ボタンがクリックされました。';
+});
