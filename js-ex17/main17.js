3c3ebb4286c79901bae2348ccc0c971cca1e26cf
tunyarzartoe-coding <htunyarzar2001@gmail.com>
2026-06-08 11:02:40 +0900

    [done] js-ex17

---
+++ js-ex17/main17.js
@@ -0,0 +1,11 @@
+﻿'use strict';
+
+const number = document.getElementById('num');
+const button = document.getElementById('round-off');
+// 四捨五入
+button.addEventListener('click', () => {
+    const num = parseFloat(number.value);
+    const rounded = Math.round(num);
+    document.getElementById('round').textContent = `四捨五入: ${rounded}`;
+    // alert(`四捨五入: ${rounded}`);
+});
