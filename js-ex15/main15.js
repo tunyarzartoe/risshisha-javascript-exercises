7d470ec83f8911cc2a836c912847a7ad4838198d
tunyarzartoe-coding <htunyarzar2001@gmail.com>
2026-06-08 10:44:26 +0900

    [done] js-ex15

---
+++ js-ex15/main15.js
@@ -0,0 +1,11 @@
+﻿'use strict';
+
+const num1 = document.getElementById('num1');
+const num2 = document.getElementById('num2');
+const button = document.getElementById('addButton');
+const result = document.getElementById('msg');
+button.addEventListener('click', () => {
+    const sum = parseInt(num1.value) + parseInt(num2.value);
+    console.log(sum);
+    result.textContent = sum;
+});
