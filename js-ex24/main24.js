9b82427e42002b0a82e782022864523e1cf8d3be
tunyarzartoe-coding <htunyarzar2001@gmail.com>
2026-06-10 14:26:35 +0900

    [done] js-ex24

---
+++ js-ex24/main24.js
@@ -0,0 +1,14 @@
+﻿// 'use strict';
+
+'use strict';
+document.querySelectorAll('#food,#drink').forEach(sec=>{
+  const ul = Array.from(sec.children).find(c=>c.tagName==='UL');
+  if(!ul) return;
+  ul.classList.add('hidden');
+  sec.querySelectorAll('img').forEach(img=>
+    img.addEventListener('click',()=>ul.classList[img.name==='open'?'remove':'add']('hidden'))
+  );
+});
+
+
+
--- main24.js
+++
@@ -1,6 +0,0 @@
-﻿'use strict';
-
-const uls = document.querySelectorAll('#food ul, #drink ul');
-uls.forEach((ul, index) => {
-  ul.classList.add('hidden');
-});
