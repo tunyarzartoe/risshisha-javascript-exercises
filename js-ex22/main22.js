e819ec95a1478ec235e436bb0e8f692861b5a095
tunyarzartoe-coding <htunyarzar2001@gmail.com>
2026-06-10 13:49:47 +0900

    [done] js-ex22

---
+++ js-ex22/main22.js
@@ -0,0 +1,13 @@
+﻿'use strict';
+
+const inputs = document.querySelectorAll('input[type="text"]');
+
+inputs.forEach((el, idx) => {
+	el.addEventListener('focus', () => {
+		el.style.backgroundColor = 'pink';
+	});
+	el.addEventListener('blur', () => {
+		el.style.backgroundColor = '#fffee8';
+	});
+	el.style.transition = 'background-color 0.12s ease-in-out';
+});
