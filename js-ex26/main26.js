1305cdbc89b87d82155499f7aa4785012ec01f9b
tunyarzartoe-coding <htunyarzar2001@gmail.com>
2026-06-11 21:14:29 +0900

    [done] js-ex26

---
+++ js-ex26/main26.js
@@ -0,0 +1,14 @@
+﻿'use strict';
+
+ const human = document.getElementById('human');
+ document.querySelectorAll('#clothes img').forEach(img=>
+   img.addEventListener('mouseover',()=>{  
+        const clone = img.cloneNode();  
+        clone.style.zIndex = 1;
+        human.parentNode.appendChild(clone);
+        clone.addEventListener('mouseout',()=>clone.remove());
+   })
+    );
+    
+
+  
