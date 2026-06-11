9494f64a3ef0d383cc3061217d1d0c25606cd7fa
tunyarzartoe-coding <htunyarzar2001@gmail.com>
2026-06-11 21:04:02 +0900

    [done] js-ex25

---
+++ js-ex25/main25.js
@@ -0,0 +1,9 @@
+﻿'use strict';
+
+const nav = document.getElementById('auto-close-nav');
+if (nav) nav.classList.remove('open');
+document.addEventListener('mousemove', e => {
+  if (e.clientY < 50) nav.classList.add('open');
+  else nav.classList.remove('open');
+});
+
